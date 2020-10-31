300041cd4e882b43f07295e009f5a2a285416aee
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-31 12:35:47 -0400

    adds dynamic navbar for login/logout

--- foodbook_client/src/components/NavBar/NavBar.js
+++ foodbook_client/src/components/NavBar/NavBar.js
@@ -4,6 +4,10 @@ import {NavLink} from 'react-router-dom';
 import ModalContainer from '../Modal/ModalContainer';
 import SearchModel from '../../models/SearchModel';
 import ResultCard from '../Results/ResultCard';
+import UserModel from '../../models/UserModel';
+
+import { useRecoilState } from "recoil";
+import { userState } from "../../recoil/atoms";
 
 import './NavBar.css';
 
@@ -11,7 +15,16 @@ const NavBar = (props) => {
     const [active, setActive] = useState(false);
     const [results, setResults] = useState([]);
     const [query, setQuery]= useState("");
+    const [user, setUser] = useRecoilState(userState);
 
+    useEffect(function () {
+        if(localStorage.getItem('uid')) {
+            UserModel.show().then((response) => {
+                setUser(response.data);
+            });
+        }
+    }, []);
+    
     useEffect(
         function () {
             if(!query) {
@@ -22,6 +35,11 @@ const NavBar = (props) => {
         },
         [query]
     );
+
+    function logout () {
+        setUser(null);
+        localStorage.clear();
+    };
     
     function fetchRecipes (query) {
         console.log("query when search begins", query);
@@ -36,7 +54,7 @@ const NavBar = (props) => {
             });
     };
 
-    function getId (string) {
+    function getRecipeId (string) {
         return string.split("recipe_")[1];
     }
 
@@ -68,17 +86,30 @@ const NavBar = (props) => {
                         </li>
                         {/* TODO once user auth is set up, add conditional logic here to display Profile and Logout links */}
                         {/* Here is where the modal containers will go */}
-                        <li className='nav-item'>
-                            <ModalContainer triggerText={"Login"} />
-                        </li>
-                        <li className='nav-item'>
-                            <ModalContainer triggerText={"Sign Up"} />
-                        </li>
+                        {user ? (
+                            <>
+                                <li className='nav-item active'>
+                                    <NavLink className='nav-link' to={`/user/${user._id}`}>My Profile</NavLink>
+                                </li>
+                                <li className='nav-item active nav-link' onClick={logout}>
+                                    Logout
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className='nav-item'>
+                                    <ModalContainer triggerText={"Login"} />
+                                </li>
+                                <li className='nav-item'>
+                                    <ModalContainer triggerText={"Sign Up"} />
+                                </li>
+                            </>
+                        )}
                     </ul>
                 </div>
             </nav>
             <div className="d-flex flex-wrap justify-content-around overflow-auto">
-                {results ? results.map((result) => <ResultCard setActive={setActive} setResults={setResults} title={result.recipe.label} source={result.recipe.source} imageUrl={result.recipe.image} key={getId(result.recipe.uri)} edamam_id={getId(result.recipe.uri)} />) : null}
+                {results ? results.map((result) => <ResultCard setActive={setActive} setResults={setResults} title={result.recipe.label} source={result.recipe.source} imageUrl={result.recipe.image} key={getRecipeId(result.recipe.uri)} edamam_id={getRecipeId(result.recipe.uri)} />) : null}
             </div>
             {active ? (<button onClick={(e) => {
                 setActive(false);
